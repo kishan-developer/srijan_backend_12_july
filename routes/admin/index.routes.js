47e75220e33a284dbba30db9c3cfc08bb2a7fd46
kishan-developer <139308962+kishan-developer@users.noreply.github.com>
2025-07-14 16:09:26 +0530

    Update index.routes.js

--- routes/admin/index.routes.js
+++ routes/admin/index.routes.js
@@ -25,4 +25,15 @@ adminRouter.use("/orders", orderRouter);
 adminRouter.use("/coupon", couponRouter);
 adminRouter.use("/offer", adminOfferRouter);
 adminRouter.get("/overview", getOverview);
+adminRouter.get('/newsletters',async(req,res)=>{
+    try {
+        const emails = await NewsletterModel.find({});
+        return res.status(200).json(emails)
+    } catch (error) {
+       return  res.status(500).json({
+            message:"Error wile fetching  emails"
+        })
+    }
+})
+
 module.exports = adminRouter;
